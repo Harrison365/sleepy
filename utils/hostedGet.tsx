36629a8f5d2916b297e717be6ef90bd5fc23f12f
Harrison Royds <harrisonroyds@yahoo.co.uk>
2024-04-24 17:56:30 +0100

    a

--- utils/hostedGet.tsx
+++ utils/hostedGet.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 const hostedGet = async (
   method: string,
   url: string,
-  setResponse: (data: any) => null
+  setResponse: (data: any) => void
 ) => {
   const { data } = await axios({
     method,
